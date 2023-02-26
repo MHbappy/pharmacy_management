28b1ba3e4542e35fd4c35e758f26baf4fa0afac3
mhbappy <mhbappy18@gmail.com>
2023-02-26 09:33:21 +0600

    added

--- src/main/java/com/pharmacy/management/model/enumeration/ROLE.java
+++ src/main/java/com/pharmacy/management/model/enumeration/ROLE.java
@@ -1,12 +1,8 @@
 package com.pharmacy.management.model.enumeration;
 
-/**
+ /**
  * The ROLE enumeration.
- */
+ **/
 public enum ROLE {
-    ADMIN,
-    MEDICAL_STUFF,
-    SERVICE_CO_ORDINATOR,
-    TECHNICAL_STAFF,
-    EMPLOYEE,
+    ADMIN, MEDICAL_STUFF, SERVICE_CO_ORDINATOR, TECHNICAL_STAFF, EMPLOYEE,
 }
