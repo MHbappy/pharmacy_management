12d44d27bf0a0b89e510ebc2dcafa2ac14992424
MHbappy <mhbappy18@gmail.com>
2023-04-10 10:42:39 +0600

    working with excel

--- src/main/java/com/pharmacy/management/config/ExcelHelper.java
+++ src/main/java/com/pharmacy/management/config/ExcelHelper.java
@@ -83,7 +83,6 @@ public class ExcelHelper {
                         default:
                             break;
                     }
-
                     cellIdx++;
                 }
             }
